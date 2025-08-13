56d6e3bff3f35fa0bc7a150ce3058abfa57658e8
Boris De Vos <143942306+borisdevos@users.noreply.github.com>
2025-08-13 16:05:11 +0200

    Triangle equation check (#28)
    * add triangle tests
    
    * update format checker
    
    * runic format
    
    * delete .JuliaFormatter.toml
    
    * bump version
    
    * Revert "bump version"
    
    This reverts commit b66df02b43eeae6665d7522e64e6573d3b536d4d.
    
    * Revert "delete .JuliaFormatter.toml"
    
    This reverts commit 97acb4c626407772e83d3c98e532e5812e06e44b.
    
    * Revert "runic format"
    
    This reverts commit 8594f824c94c4e1a5d133c7c83e721b6553cc2ac.
    
    * Revert "update format checker"
    
    This reverts commit 79ce194162a371e60b376205781612bb65eaa187.
    
    * increase compat TensorKitSectors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using CategoryData
 using Test, TestExtras, Random
 using TensorKitSectors
-using TensorKitSectors: fusiontensor, pentagon_equation, hexagon_equation
+using TensorKitSectors: fusiontensor, triangle_equation, pentagon_equation, hexagon_equation
 using Base.Iterators: product
 using LinearAlgebra: LinearAlgebra
 using TensorOperations: @tensor
