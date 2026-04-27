f85c80a547760aa36e00dfc541b28b55a77de446
Chenqi Meng <mcqgravity@gmail.com>
2026-04-27 23:34:40 +0800

    Added some fusion categories

--- src/aliases.jl
+++ src/aliases.jl
@@ -39,6 +39,50 @@ const VecS3 = VecD3
 Base.getindex(::CenterTable, ::Type{VecS3}) = ZVecS3
 @objectnames ZVecS3 = ZVecS3 A B C F G H D E
 
+@objectnames VecZ2 = UFC{2, 1, 0, 1, 0} _0 _1
+@objectnames VecZ2ω = UFC{2, 1, 0, 1, 1} _0 _1
+@objectnames Fib_fusion = UFC{2, 1, 0, 1, 1} I τ
+@objectnames RepS3_fusion = UFC{3, 1, 0, 2, 0} I a b σ # How to understand UFC{3, 1, 0, 2, 1} and UFC{3, 1, 0, 2, 2}? They have the same fusion rule as RepS3, but different associator. They are not braided, thus cannot be viewed as RepS3 with different braiding.
+@objectnames VecZ3 = UFC{3, 1, 2, 1, 0} _0 _1 _2
+@objectnames VecZ3ω_1 = UFC{3, 1, 2, 1, 1} _0 _1 _2
+@objectnames VecZ3ω_2 = UFC{3, 1, 2, 1, 2} _0 _1 _2
+@objectnames PSU2_5_fusion = UFC{3, 1, 0, 3, 0} _0 _4 _2
+
+@objectnames VecZ2xZ2 = UFC{4, 1, 0, 1, 0} I a b c
+@objectnames VecZ2ωxVecZ2ω = UFC{4, 1, 0, 1, 1} I a b c
+@objectnames FibxVecZ2 = UFC{4, 1, 0, 2, 0} I0 I1 τ1 τ0
+@objectnames FibxVecZ2ω = UFC{4, 1, 0, 2, 1} I0 I1 τ1 τ0
+@objectnames RepD5_fusion = UFC{4, 1, 0, 3, 0} I a σ1 σ2 # How to understand UFC{4, 1, 0, 3, 1} and UFC{4, 1, 0, 3, 2}? It has the same fusion rule as RepD5, but different associator. It is not braided, thus cannot be viewed as RepD5 with different braiding.
+@objectnames PSU2_6_fusion = UFC{4, 1, 0, 4, 0} _0 _2 _4 _6
+@objectnames FibFib_fusion = UFC{4, 1, 0, 5, 0} II τI Iτ ττ
+@objectnames PSU2_7_fusion = UFC{4, 1, 0, 6, 0} _0 _2 _4 _6
+@objectnames VecZ4 = UFC{4, 1, 2, 1, 0} _0 _2 _1 _3
+@objectnames VecZ4ω1 = UFC{4, 1, 2, 1, 1} _0 _2 _1 _3
+@objectnames VecZ4ω2 = UFC{4, 1, 2, 1, 2} _0 _2 _1 _3
+@objectnames VecZ4ω3 = UFC{4, 1, 2, 1, 3} _0 _2 _1 _3
+@objectnames TYZ3_1 = UFC{4, 1, 2, 2, 0} _0 _1 _2 σ # TY needs further identifications
+@objectnames TYZ3_2 = UFC{4, 1, 2, 2, 1} _0 _1 _2 σ # TY needs further identifications
+@objectnames TYZ3_3 = UFC{4, 1, 2, 2, 2} _0 _1 _2 σ # TY needs further identifications
+@objectnames TYZ3_4 = UFC{4, 1, 2, 2, 3} _0 _1 _2 σ # TY needs further identifications
+# How to understand UFC{4, 1, 2, 4, 0} and UFC{4, 1, 2, 4, 1}? dims: 1, 1, 1 + √2, 1 + √2
+
+@objectnames SU2_4_fusion = UFC{5, 1, 0, 3, 0} _0 _4 _3 _1 _2
+@objectnames JK4_fusion = UFC{5, 1, 0, 3, 1} _0 _4 _3 _1 _2
+@objectnames RepD7_fusion = UFC{5, 1, 0, 4, 0} I a _16 _25 _34 # Needs further test fusion rule. How to understand UFC{5, 1, 0, 4, 1}? It has the same fusion rule as RepD7, but different associator. It is not braided, thus cannot be viewed as RepD7 with different braiding.
+@objectnames RepS4_fusion = UFC{5, 1, 0, 6, 1} I a b σ τ # Needs further test fusion rule. How to understand UFC{5, 1, 0, 6, 0}? It has the same fusion rule as RepS4, but different associator. It is not braided, thus cannot be viewed as RepS4 with different braiding.
+@objectnames PSU2_8_fusion = UFC{5, 1, 0, 7, 0} _0 _8 _2 _6 _4
+@objectnames PSU2_9_fusion = UFC{5, 1, 0, 10, 0} _0 _8 _2 _6 _4
+@objectnames TYZ4_1 = UFC{5, 1, 2, 1, 0} _0 _1 _2 _3 σ # TY needs further identifications
+@objectnames TYZ4_2 = UFC{5, 1, 2, 1, 1} _0 _1 _2 _3 σ # TY needs further identifications
+@objectnames TYZ4_3 = UFC{5, 1, 2, 1, 2} _0 _1 _2 _3 σ # TY needs further identifications
+@objectnames TYZ4_4 = UFC{5, 1, 2, 1, 3} _0 _1 _2 _3 σ # TY needs further identifications
+# How to understand UFC{5, 1, 2, 3, 0} and UFC{5, 1, 2, 3, 1}? dims: 1, 1, √3, √3, 2. Same set of qdims as SU2_4, but different duality structure. Perhaps some ℤ₂-equivariantization of TYZ3's.
+# How to understand UFC{5, 1, 2, 4, 0} and UFC{5, 1, 2, 4, 1}? dims: 1, 1, 2, 3, 3. Same set of qdims as RepS4, but different duality structure. Perhaps also S₃-equivariantization of some other VecZ2xZ2ω
+@objectnames VecZ5 = UFC{5, 1, 4, 1, 0} _0 _1 _4 _3 _2
+@objectnames VecZ5ω_1 = UFC{5, 1, 4, 1, 1} _0 _1 _4 _3 _2
+@objectnames VecZ5ω_2 = UFC{5, 1, 4, 1, 2} _0 _1 _4 _3 _2
+@objectnames PSU2_9_fusion = UFC{5, 1, 0, 10, 0} _0 _8 _2 _6 _4
+
 # Varia
 # Notation: 𝒞⁻: braided reversed of 𝒞; 𝒞ᴳG: G-equivariantization of 𝒞. There may exist several inequivalent equivariantiazations. We use numbers to indicate them.
 @objectnames Fib⁻ = PMFC{2, 1, 0, 2, 0, 0} I τ
@@ -47,8 +91,8 @@ Base.getindex(::CenterTable, ::Type{VecS3}) = ZVecS3
 @objectnames Semion⁻ = PMFC{2, 1, 0, 1, 1, 1} I ϵ
 @objectnames sVec = PMFC{2, 1, 0, 1, 0, 1} I ψ # Non-modular
 
-@objectnames Z3MTC = PMFC{3, 1, 2, 1, 0, 1} _0 _1 _2
-@objectnames Z3MTC⁻ = PMFC{3, 1, 2, 1, 0, 2} _0 _1 _2
+@objectnames Z3MTC = PMFC{3, 1, 2, 1, 0, 2} _0 _1 _2
+@objectnames Z3MTC⁻ = PMFC{3, 1, 2, 1, 0, 1} _0 _1 _2
 @objectnames Ising = PMFC{3, 1, 0, 1, 1, 3} I ψ σ
 @objectnames Ising3 = PMFC{3, 1, 0, 1, 0, 3} I ψ σ # c = 3//2 Ising type UMTC
 @objectnames Ising5 = PMFC{3, 1, 0, 1, 0, 0} I ψ σ # c = 5//2 Ising type UMTC
