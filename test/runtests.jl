041f0dec44f96f5d7b5d02591f615ae6a7d860e3
Boris De Vos <boris.devos@ugent.be>
2026-06-17 15:54:47 +0200

    modules are hard

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,7 @@ using .SectorTestSuite: randsector
 SectorTestSuite.smallset(::Type{I}) where {I <: Object} = collect(values(I))
 #TODO: remove when new TKS patch gets released
 function SectorTestSuite.randsector(::Type{I}) where {I <: Object}
-    s = collect(smallset(I))
+    s = collect(SectorTestSuite.smallset(I))
     a = Random.rand(s)
     if Base.IteratorSize(values(I)) === Base.HasLength()
         length(values(I)) == 1 && return a
